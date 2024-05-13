da20d123f0fcfedb20972d3e9c86ba4194695f4f
Arulkumara Babu <22z310@psgtech.ac.in>
2024-05-13 19:31:51 +0530

    artist value insertion complete

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise'); 
 const app = express();
 const pool = mysql.createPool({
   host: "127.0.0.1",
@@ -13,20 +13,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/form', (req, res) => {
     res.sendFile(__dirname + '/dash.html');
-})
+});
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/form', (req, res) => {
-    const { name, email, phone, dob, address } = req.body;
+async function generateId(name, email, phone, dob, address, style) {
+    try {
+        const [rows, fields] = await pool.query('SELECT id FROM art.person WHERE name = ? AND email = ? AND dob = ? AND address = ?', [name, email, dob, address]);
+        
+        if (rows.length > 0) {
+            return -1;
+        }
+        
+        const [countRows] = await pool.query('SELECT COUNT(*) as count FROM art.person');
+        const count = countRows[0].count;
+        const id = 'A' + ('000' + (count + 1)).slice(-3);
+        return id;
+    } catch (error) {
+        throw error; 
+    }
+}
+
+app.post('/form', async (req, res) => {
+    const { name, email, phone, dob, address, style } = req.body;
 
     try {
-        pool.query('INSERT INTO art.artists (name, email, phone, dob, address) VALUES (?, ?, ?, ?, ?)', [name, email, phone, dob, address]);
+        const id = await generateId(name, email, phone, dob, address, style); 
+        
+        if (id != -1) {
+            await pool.query('INSERT into art.person (id, name, dob, address, email) VALUES (?, ?, ?, ?, ?)', [id, name, dob, address, email]);
+            await pool.query('INSERT into art.artist (artistId, style) VALUES (?, ?)', [id, style]);
+            await pool.query('INSERT into art.phonenumber (id, phone) VALUES (?, ?)', [id, phone]);
+        }
     } catch (error) {
         console.error('Error:', error);
+        res.status(500).send('An error occurred while processing your request.');
     }
-})
+});
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
